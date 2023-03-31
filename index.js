050a3e81471d304fae5c5fa11a07143bf7b50278
Eva Camino <caminoeva1@gmail.com>
2023-03-31 13:51:04 -0600

    starting

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+class Manager {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+  getRole() {
+    return "Manager";
+  }
+}
+
+
+class Intern {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+  getSchool() {
+    return this.school;
+  }
+  getRole() {
+    return "Intern";
+  }
+}
+
+module.exports = {Manager,Engineer,Intern}
+
+//inquierer
