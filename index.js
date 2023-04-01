26adb666ff7c7d5dfa198e41b8c4c0bad5b8eea2
Eva Camino <caminoeva1@gmail.com>
2023-04-01 11:17:54 -0600

    index.js function

--- index.js
+++ index.js
@@ -1,46 +1,95 @@
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
 
+const managerPrompts = [
+  {
+    type: "input",
+    name: "id",
+    message: "What is your id?",
+  },
+  {
+    type: "input",
+    name: "managerName",
+    message: "What is your Manager's name?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is your office number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+];
+const engineerPrompts = [
+  {
+    type: "input",
+    name: "id",
+    message: "What is your id?",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is your Engineer's name?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+];
 
-class Manager {
-  constructor(name, id, email, officeNumber) {
-    super(name, id, email);
-    this.officeNumber = officeNumber;
-  }
-  getOfficeNumber() {
-    return this.officeNumber;
-  }
-  getRole() {
-    return "Manager";
-  }
+const internPrompts = [
+  {
+    type: "input",
+    name: "id",
+    message: "What is your id?",
+  },
+  {
+    type: "input",
+    name: "internName",
+    message: "What is your intern's name?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is your school name?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+];
+
+async function promptManager() {
+  await inquirer.prompt(managerPrompts).then(function (answers) {
+    console.log(answers);
+  });
 }
+promptManager();
 
-
-class Intern {
-  constructor(name, id, email, school) {
-    super(name, id, email);
-    this.school = school;
-  }
-  getSchool() {
-    return this.school;
-  }
-  getRole() {
-    return "Intern";
-  }
+async function promptIntern(){
+  await inquirer.prompt(internPrompts).then(function(answers){
+    console.log(answers)
+  })
 }
-
-class Engineer {
-  constructor(name,id,email, github) {
-    super(name, id, email);
-    this.github = github
-  }
-  getGithub() {
-    return this.github;
-  }
-  getRole() {
-    return "Enginer"
-  }
-}
-
-
-module.exports = {Manager,Engineer,Intern}
-
+promptIntern();
+// function promptEngineer(){
+//   inquirer.prompt(engineerPrompts).then(function(answers){
+//     console.log(answers)
+//   })
+// }
+// promptEngineer();
 //inquierer
+// module.exports = Employee
