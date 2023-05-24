a0a88d8d0c714c1faf5722f5ac510b6941962ae0
Eva Camino <caminoeva1@gmail.com>
2023-05-24 00:05:44 -0600

    adding src

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -4,14 +4,14 @@ describe('Employee Test Suite', ()=>{
         it("can get values from manager constructor", () => {
             const employee = new Manager('Jim','12','asifjasdfo@gmail.com','1234567890');
             
-            expect(employee.getOfficenumber()).toBe('1234567890');
-            expect(employee.officenumber.length).toEqual(10)
+            expect(employee.getofficeNumber()).toBe('1234567890');
+            expect(employee.officeNumber.length).toEqual(10)
             expect(employee.getRole()).toBe('Manager')
         })
         it("Can set values", () => {
             const employee = new Manager('Jim','12','asifjasdfo@gmail.com','1234567890');
             expect(typeof employee.id).toBe('string')
-            expect(employee.officenumber).toBe('1234567890');
+            expect(employee.officeNumber).toBe('1234567890');
         })
 
 })
--- index.js
+++ index.js
@@ -4,34 +4,48 @@ const Engineer = require("./lib/Engineer");
 const inquirer = require("inquirer");
 const Employee = require("./lib/Employee");
 const EmployeeList = [];
+const path = require("path");
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const fs = require("fs")
+const render = require("./src/html-generator.js");
 
-const managerPrompts = [
-  {
-    type: "input",
-    name: "id",
-    message: "What is your id?",
-  },
-  {
-    type: "input",
-    name: "name",
-    message: "What is your Manager's name?",
-  },
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "What is your office number?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email?",
-  },
-  {
-    type: "confirm",
-    name: "addAgain",
-    message: "Would you like to add another employee?",
-  },
-];
+function buildTeam() {
+  // Create the output directory if the output path doesn't exist
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
+  }
+  fs.writeFileSync(outputPath, render(EmployeeList), "utf-8");
+}
+
+
+// const managerPrompts = [
+//   {
+//     type: "input",
+//     name: "id",
+//     message: "What is your id?",
+//   },
+//   {
+//     type: "input",
+//     name: "name",
+//     message: "What is your Manager's name?",
+//   },
+//   {
+//     type: "input",
+//     name: "officeNumber",
+//     message: "What is your office number?",
+//   },
+//   {
+//     type: "input",
+//     name: "email",
+//     message: "What is your email?",
+//   },
+//   {
+//     type: "confirm",
+//     name: "addAgain",
+//     message: "Would you like to add another employee?",
+//   },
+// ];
 const engineerPrompts = [
   {
     type: "input",
@@ -87,42 +101,76 @@ const internPrompts = [
     message: "Would you like to add another employee?",
   },
 ];
-const mainMenu = () => {
+function createManager (){
+  console.log('lets get team started')
+inquirer.prompt( [
+  {
+    type: "input",
+    name: "id",
+    message: "What is your id?",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is your Manager's name?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is your office number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+]).then(answers=>{
+  const manager = new Manager(
+    answers.name,
+    answers.id,
+    answers.email,
+    answers.officeNumber
+  );
+  EmployeeList.push(manager)
+  employeeMenu();
+})
+  
+}
+const employeeMenu = () => {
   inquirer
     .prompt({
       type: "list",
       name: "emp_type",
       message: "select employee type",
-      choices: ["Engineer", "Intern", "Manager"],
+      choices: ["Engineer", "Intern", "None"],
     })
     .then(({ emp_type }) => {
-      if (emp_type === "Manager") {
-        promptManager();
-      } else if (emp_type === "Engineer") {
-        
-        promptEngineer();
-      } else {
+      if (emp_type === "Intern") {
         promptIntern();
+      } else if (emp_type === "Engineer"){
+        promptEngineer();
+      
+    }else {
+      buildTeam();
       }
-    });
-};
-async function promptManager() {
-  await inquirer.prompt(managerPrompts).then(function (answers) {
-    const manager = new Manager(
-      answers.name,
-      answers.id,
-      answers.email,
-      answers.officeNumber
-    );
+    })}
+// async function promptManager() {
+//   await inquirer.prompt(managerPrompts).then(function (answers) {
+//     const manager = new Manager(
+//       answers.name,
+//       answers.id,
+//       answers.email,
+//       answers.officeNumber
+//     );
 
-    EmployeeList.push(manager);
-    if (answers.addAgain) {
-      mainMenu();
-    } else {
-      console.log(EmployeeList);
-    }
-  });
-}
+//     EmployeeList.push(manager);
+//     if (answers.addAgain) {
+//       mainMenu();
+//     } else {
+//       console.log(EmployeeList);
+//     }
+//   });
+// }
 async function promptEngineer() {
   await inquirer.prompt(engineerPrompts).then(function (answers) {
     const engineer = new Engineer(
@@ -133,10 +181,12 @@ async function promptEngineer() {
     );
     EmployeeList.push(engineer);
     if (answers.addAgain) {
-      mainMenu();
+      employeeMenu();
     } else {
+      buildTeam();
       console.log(EmployeeList);
     }
+
   });
 }
 
@@ -150,8 +200,9 @@ async function promptIntern() {
     );
     EmployeeList.push(intern);
     if (answers.addAgain) {
-      mainMenu();
+      employeeMenu();
     } else {
+      buildTeam();
       console.log(EmployeeList);
     }
   });
@@ -164,8 +215,7 @@ async function promptIntern() {
 // await console.log(engineerList);
 // await console.log(internList);
 // }
-
-mainMenu();
+createManager();
 
 //inquierer
 // module.exports = Employee
